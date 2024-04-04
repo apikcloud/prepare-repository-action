426a8c004fbc59d5afe8dee90b3d2d7bdebd41f6
Aurelien ROY <roy.aurelien@gmail.com>
2024-04-04 19:24:34 +0200

    [WIP]

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ FROM alpine:3.10
 RUN apk update && apk add git
 
 COPY entrypoint.sh /entrypoint.sh
-COPY bin/* /usr/bin
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,46 @@
 ADDONS_PATH=/github/workspace
 
 echo "flatten submodules"
-flatten-submodules ${ADDONS_PATH}
+
+git -C "${ADDONS_PATH}" config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read -r KEY MODULE_PATH
+    do
+        DIR="$ADDONS_PATH/$MODULE_PATH"
+
+        if [ -d "$DIR" ]
+        then
+            if [ "$(ls -A $DIR)" ]; then
+                echo "move $DIR to $ADDONS_PATH"
+                cp -rf "${DIR}"/* "${ADDONS_PATH}"
+            else
+                echo "$DIR is Empty"
+            fi
+            rm -rf "$DIR"
+        else
+            echo "Directory $DIR not found."
+        fi
+
+    done
 
 echo "clean addons"
-clean-addons ${ADDONS_PATH}
+
+files=("Dockerfile" "README.md")
+dirs=("setup")
+
+for name in ${files[@]}; do
+    if [ -f "${ADDONS_PATH}/${name}" ]
+    then
+        rm -rf "${ADDONS_PATH}/${name}"
+    fi
+done
+
+for name in ${dirs[@]}; do
+    if [ -d "${ADDONS_PATH}/${name}" ]
+    then
+        rm -rf "${ADDONS_PATH}/${name}"
+    fi
+done
+
+# Clean dot filles
+cd "$ADDONS_PATH"
+rm -rf .[!.]* ..?*
